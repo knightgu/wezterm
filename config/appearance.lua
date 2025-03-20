a8809819feecc5d4e14da34911971e5a65d4804c
Yuwen Gu <knightgu34@gmail.com>
2025-03-19 22:24:23 -0400

    Turn off tab icons

--- config/appearance.lua
+++ config/appearance.lua
@@ -30,7 +30,7 @@ return {
    enable_scroll_bar = true,
 
    -- tab bar
-   enable_tab_bar = true,
+   enable_tab_bar = false,
    hide_tab_bar_if_only_one_tab = true,
    use_fancy_tab_bar = false,
    tab_max_width = 25,
